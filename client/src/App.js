f7c66eb027a61520a80d1953fe8916d1fc10e4ab
aram1l7 <72884813+aram1l7@users.noreply.github.com>
2022-07-14 01:17:32 +0400

    Fix router redirect small bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import Accounts from "./pages/accounts";
 import Account from "./pages/account";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 function App() {
-  useEffect(() => {
-    window.history.pushState({}, undefined, "/accounts");
-  }, []);
   const theme = createTheme();
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/accounts" />} />
           <Route path="/accounts" element={<Accounts />} />
           <Route path="/accounts/:id" element={<Account />} />
         </Routes>
